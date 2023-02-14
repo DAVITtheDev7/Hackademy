506b425e7adfa33a673445449ff23fb680e23b23
DaviDM2005 <87268065+DaviDM2005@users.noreply.github.com>
2023-02-14 20:25:01 +0400

    test

--- index.js
+++ index.js
@@ -5,3 +5,26 @@ function feedback() {
   
 let submitButton = document.getElementById("submit-button");
 submitButton.addEventListener("click", feedback);
+
+
+
+var inputs = document.querySelectorAll(".my-input");
+
+for (var i = 0; i < inputs.length; i++) {
+  var input = inputs[i];
+
+ 
+  input.addEventListener("focus", function() {
+    this.style.backgroundColor = "black"; 
+  });
+
+ 
+  input.addEventListener("blur", function() {
+    this.style.backgroundColor = ""; 
+  });
+
+ 
+  input.addEventListener("input", function() {
+    this.style.backgroundColor = "yellow"; 
+  });
+}
