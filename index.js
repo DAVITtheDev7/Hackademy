3140efc2387c19c26ca793576bb74a83ca8745a5
DaviDM2005 <87268065+DaviDM2005@users.noreply.github.com>
2023-02-21 07:26:22 +0400

    test

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ window.addEventListener('load', function() {
   const loader = document.getElementById('loader');
   setTimeout(function() {
     loader.style.display = 'none';
-  }, 2000)
+  }, 1000)
 });
 
 
