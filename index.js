1eed95ddac4873912368578f919dc7354250dfaa
DaviDM2005 <87268065+DaviDM2005@users.noreply.github.com>
2023-02-25 02:55:32 +0400

    5.7.0

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-function feedback() {
-    let name = document.getElementById('name').value;
-    alert("Thank you " + name + " for your feedback.");
-  }
-  
-let submitButton = document.getElementById("submit-button");
-submitButton.addEventListener("click", feedback);
-
-
-
-
 // Navbar
 
 const buttons = document.querySelectorAll('.nav-bar li a');
