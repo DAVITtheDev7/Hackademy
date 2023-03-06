f4e29fabd1a82c52b8ac088d434a38762e009622
DaviDM2005 <87268065+DaviDM2005@users.noreply.github.com>
2023-03-07 01:35:52 +0400

    7.0.0

---
+++ courses/app.js
@@ -0,0 +1,30 @@
+const searchInput = document.querySelector('#search-input');
+const list = document.querySelector('#list');
+const nothingFound = document.createElement('div');
+nothingFound.textContent = 'Nothing was found';
+nothingFound.style.display = 'none';
+list.appendChild(nothingFound);
+
+searchInput.addEventListener('input', (event) => {
+  const searchTerm = event.target.value.toLowerCase();
+  let numMatches = 0;
+
+  Array.from(list.children).forEach((item) => {
+    const textContent = item.textContent.toLowerCase();
+    if (textContent.includes(searchTerm)) {
+      item.style.display = 'block';
+      item.style.color = '#d15c04';
+      numMatches++;
+    } else {
+      item.style.display = 'none';
+    }
+  });
+
+  if (numMatches === 0) {
+    nothingFound.style.display = 'block';
+    document.querySelector('#course-sec').style.height = '95vh';
+  } else {
+    nothingFound.style.display = 'none';
+    document.querySelector('#course-sec').style.height = '';
+  }
+});
